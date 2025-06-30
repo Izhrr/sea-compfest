ae413b91988a7ef22b7c94411b42e6e1da8a8ced
Izhrr <18223129@std.stei.itb.ac.id>
2025-07-01 06:51:05 +0700

    feat: navbar desktop

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,22 @@ import {navLinks} from "../constants"
 
 const Nav = () => {
   return (
-    <header>
-        <nav>
-            Navbar
+    <header className="fixed z-999 px-15 py-5 backdrop-blur-md w-full">
+        <nav className="flex justify-between items-center">
+            <a href="/" className="font-heading text-h5 text-primary">
+              SEA Catering
+            </a>
+
+            <ul className="flex justify-center items-center gap-16 max-lg:hidden">
+              {navLinks.map((item)=>(
+                <li key={item.label}>
+                  <a href={item.href} className="font-paragraph text-paragraph-black">
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
         </nav>
     </header>
   )
