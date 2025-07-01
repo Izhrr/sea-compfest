2b4dbf14cf823fc30af494c8c3d37055c5be34e9
Izhrr <18223129@std.stei.itb.ac.id>
2025-07-01 08:46:18 +0700

    feat: create card component

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,6 +1,26 @@
-const ReviewCard = () => {
+const ReviewCard = ({ratingImgURL, title, feedback, reviewerName}) => {
   return (
-    <div>ReviewCard</div>
+    <div className="flex flex-col gap-2 items-start p-6 bg-white shadow-lg rounded-xl w-full md:w-[calc(50%-theme(gap.8)/2)] lg:w-[calc(33.33%-theme(gap.8)*2/3)] min-w-0">
+      {ratingImgURL && (
+        <div>
+          <img
+            src={ratingImgURL}
+            alt="Customer rating stars"
+            className="w-auto h-6 object-contain"
+          />
+        </div>
+      )}
+
+      <h3 className="font-subheading text-h4 leading-h4 text-paragraph-black"> {title}</h3>
+
+      <p className="font-paragraph text-p3 leading-p3 text-paragraph-black"> 
+        {feedback}
+      </p>
+
+      <p className="font-heading text-p2 leading-p2 text-paragraf-black">
+        - {reviewerName}
+      </p>
+    </div>
   )
 }
 
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,12 +1,16 @@
-const ServiceCard = () => {
+const ServiceCard = ({iconURL, label}) => {
   return (
-    <div className="flex items-center bg-white border border-[#d0e7d6] rounded-lg px-5 py-4 shadow-sm">
-      <span className="bg-[#d0e7d6] rounded-full p-2 mr-4 flex items-center justify-center">
-        {icon}
-      </span>
-      <span className="text-base md:text-lg text-gray-800 font-medium">
-        {text}
-      </span>
+    <div className="flex gap-4 items-center p-5 bg-white shadow-lg rounded-lg">
+      {iconURL && (
+        <div>
+          <img
+            src={iconURL}
+            alt="Service-Icon"
+            className="object-contain"
+          />
+        </div>
+      )}
+      <p className="font-paragraph text-p2 text-paragraph-black">{label}</p>
     </div>
   )
 }
